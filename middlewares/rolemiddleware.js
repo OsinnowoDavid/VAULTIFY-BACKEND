a1ad53078afdd041063a95a974668ea91e311c9a
OsinnowoDavid <davidadewale151@gmail.com>
2025-04-18 18:15:42 +0100

    admin

--- middlewares/rolemiddleware.js
+++ middlewares/rolemiddleware.js
@@ -1,10 +1,26 @@
-const authorizationRole = (...allowroles) => {
+const authorizationRole = (...allowedRoles) => {
     return (req, res, next) => {
-        if (!allowroles.includes(req.user.adminRole)) {
-            return res.status(403).json({ message: "You are not authorized to access this route" });
-        }
-        next();
+      if (!allowedRoles.includes(req.user.adminRole)) {
+        return res.status(403).json({ message: "You are not authorized to access this route" });
+      }
+  
+      // If user has the required role, allow the request to proceed
+      next();
     };
-};
+  };
+  
+  export default authorizationRole;
+  
 
-export default authorizationRole;
+// const authorizationRole = (...allowroles) => {
+//     return (req, res, next) => {
+//         if (!allowroles.includes(req.user.adminRole)) {
+//             return res.status(403).json({ message: "You are not authorized to access this route" });
+//         }else{
+//             return res.status(200).json({ message: "You are authorized to access this route" });
+//         }
+//         next();
+//     };
+// };
+
+// export default authorizationRole;
