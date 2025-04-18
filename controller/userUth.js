31240aabf5549ea2631eb853b781bd85fde114c6
OsinnowoDavid <davidadewale151@gmail.com>
2025-04-18 18:07:11 +0100

    admin

--- controller/userUth.js
+++ controller/userUth.js
@@ -1,41 +1,41 @@
-import dotenv from "dotenv"
-import jwt from "jsonwebtoken"
-dotenv.config()
-const userAuth = (req, res, next) => {
-  const token = req.cookies;
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(403).json({ message: "Invalid token" });
-  }
-};
-export  {userAuth}
-
-
+// import dotenv from "dotenv"
 // import jwt from "jsonwebtoken"
-// const userAuth = async ( req,res, next) =>{
-// const {token} = req.cookies;
-// if (!token ){
-//  return res.json({success:false, message:"not authorized, Login Again"})
-// }
-// try {
-//   const tokenDecode=  jwt.verify(token, process.env.JWT_SECRET)
+// dotenv.config()
+// const userAuth = (req, res, next) => {
+//   const token = req.cookiesy;
+//   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-//   if(tokenDecode.id){
-//     req.body.userId = tokenDecode.id
-//   } else{ return res.json({success:false, message:"not authorized login again "})
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch (err) {
+//     return res.status(403).json({ message: "Invalid token" });
 //   }
-  
-// next()
-    
-// } catch (error) {
-//      res.json({success:false, error:message})
-    
-// }
-// }
+// };
+// export  {userAuth}
 
-// export {userAuth}
+
+import jwt from "jsonwebtoken"
+const userAuth = async ( req,res, next) =>{
+const {token} = req.cookies;
+if (!token ){
+ return res.json({success:false, message:"not authorized, Login Again"})
+}
+try {
+  const tokenDecode=  jwt.verify(token, process.env.JWT_SECRET)
+
+  if(tokenDecode.id){
+    req.body.userId = tokenDecode.id
+  } else{ return res.json({success:false, message:"not authorized login again "})
+  }
+  
+next()
+    
+} catch (error) {
+     res.json({success:false, error:message})
+    
+}
+}
+
+export {userAuth}
