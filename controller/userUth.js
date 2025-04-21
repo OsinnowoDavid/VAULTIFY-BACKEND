115848b946d438e7711d51825a8759f6e259dcb9
OsinnowoDavid <davidadewale151@gmail.com>
2025-04-21 14:04:51 +0100

    auth

--- controller/userUth.js
+++ controller/userUth.js
@@ -1,3 +1,30 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({ success: false, message: "Not authorized, Login Again" });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (tokenDecode.id) {
+      req.user = tokenDecode.id;
+      return next(); // Proceed if the token is valid
+    } else {
+      return res.status(401).json({ success: false, message: "Not authorized, Login Again" });
+    }
+  } catch (error) {
+    console.error("Error in userAuth middleware:", error);
+    return res.status(401).json({ success: false, message: "Token verification failed, Login Again" });
+  }
+};
+
+export { userAuth };
+
+
 // import dotenv from "dotenv"
 // import jwt from "jsonwebtoken"
 // dotenv.config()
@@ -16,27 +43,27 @@
 // export  {userAuth}
 
 
-import jwt from "jsonwebtoken"
-const userAuth = async ( req,res, next) =>{
-const {token} = req.cookies;
-if (!token ){
- return res.json({success:false, message:"not authorized, Login Again"})
-}
-try {
-  const tokenDecode=  jwt.verify(token, process.env.JWT_SECRET)
+// import jwt from "jsonwebtoken"
+// const userAuth = async ( req,res, next) =>{
+// const {token} = req.cookies;
+// if (!token ){
+//  return res.json({success:false, message:"not authorized, Login Again"})
+// }
+// try {
+//   const tokenDecode=  jwt.verify(token, process.env.JWT_SECRET)
 
-  if(tokenDecode.id){
-    req.user = tokenDecode.id
-  } else{ return res.json({success:false, message:"not authorized login again "})
-  }
+//   if(tokenDecode.id){
+//     req.user = tokenDecode.id
+//   } else{ return res.json({success:false, message:"not authorized login again "})
+//   }
   
-next()
+// next()
     
-} catch (error) {
-     res.json({success:false, })
-     console.log(error)
+// } catch (error) {
+//      res.json({success:false, })
+//      console.log(error)
     
-}
-}
+// }
+// }
 
-export {userAuth}
+// export {userAuth}
